62d5d30926b278fe477372629e43c2d0cb654097
ZIMA <azimatulmatsniya@gmail.com>
2021-12-22 10:33:21 +0700

    project

--- main/views.py
+++ main/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    if request.method == 'POST':
+        masukx = int(request.POST['titik x'])
+        masuky = int(request.POST['titik y'])
+        pusatx = int(request.POST['pusat x'])
+        pusaty = int(request.POST['pusat y'])
+        k = int(request.POST['skala'])
+        keluarx = k*(masukx-pusatx)+pusatx
+        keluary = k*(masuky-pusaty)+pusaty
+        keluar = [str(keluarx) + ',' + str(keluary)]
+        context = {'keluar': keluar,
+                   }
+        return render(request, 'main/home.html', context)
+
+    else:
+        return render(request, 'main/home.html', {})
